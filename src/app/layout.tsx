38434bbaad372b0175f8cacac01bef48b19e498b
Sanjida Akter Sima <simasanjida8@gmail.com>
2026-09-26 02:17:33 +0600

    import provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,26 @@
 
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
-// import { Provider } from "../components/providerss/Provider";
+import { FirlogProvider } from "../components/providerss/FitlogProvider";
 
-
-
-
-
-
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({ children }: 
+  
+  Readonly <{
+    children: React.ReactNode;
+  }>
+  // LayoutProps<"/">
+  ) 
+  {
   return (
     <html
       lang="en"
       data-theme="light">
       <body>
-
+      <FitlogProvider>
 
         <Navbar/>
         <div>{children}</div>
+       </FitlogProvider>
       </body>
     </html>
   );
