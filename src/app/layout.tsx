ae185b0f2468dca344d5538749bc89c8bec98d45
Sanjida Akter Sima <simasanjida8@gmail.com>
2026-09-26 00:54:33 +0600

    my 2nd commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
+import { Provider } from "../components/provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
---
+++ src/types/workout.tsx
@@ -0,0 +1,17 @@
+
+
+
+export interface Workout {
+    id:number;
+    name:string;
+    image:string;
+    muscleGroup:string[];
+    equopment:string;
+    difficulty:string;
+    duration:number;
+    caloriesBurned:number;
+    sets:number;
+    reps:string;
+    rating:number;
+    discription:string[];
+}
